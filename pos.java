b76e6c738908478b1fb6bcff4a4c1559fd6156a8
karthika1997 <karthikasangeetha5@gmail.com>
2017-06-05 17:51:11 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class pos
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n==0)
+{
+System.out.println("zero");
+}else if(n>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
