aa812497eb212728a334596249f09338c605f159
karthika1997 <karthikasangeetha5@gmail.com>
2017-06-06 15:00:31 +0530

    Update check.java

--- check.java
+++ check.java
@@ -6,13 +6,20 @@ public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
+if(n>0)
+{
 if(n%2==0)
 {
 System.out.println("even");
 }
-else
+else(n%2!=0)
 {
 System.out.println("odd");
 }
+}
+  else
+  {
+    System.out.println("invalid");
+  }
 }
 }
