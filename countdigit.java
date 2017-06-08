cc0d592722d7ade6279c4aa9cc5cbeac28eadef3
karthika1997 <karthikasangeetha5@gmail.com>
2017-06-08 18:42:32 +0530

    Create countdigit.java

---
+++ countdigit.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class countdigit
+{
+public static void main(String args[])
+{
+int i,count=0;
+String s="12345";
+int c=s.length();
+for(i=0;i<c;i++)
+{
+count++;
+}
+System.out.println(count);
+}
+}
+
