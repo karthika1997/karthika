a93027645acfb460da8361308c3f66877797c029
karthika1997 <karthikasangeetha5@gmail.com>
2017-06-08 17:40:52 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.Scanner;
+class vowel
+{
+public static void main(String args[])
+{
+char ch='a';
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+System.out.println("vowel");}
+else
+{
+System.out.println("consonence");
+}
+}
+}
