b9a67fb8036ba2c8119b49fb2951968223ba0f53
karthika1997 <karthikasangeetha5@gmail.com>
2017-06-08 17:51:10 +0530

    Create vowel.java

--- vowel.java
+++ vowel.java
@@ -6,6 +6,8 @@ class vowel
 public static void main(String args[])
 {
 char ch='a';
+if(ch!=0)
+{
 if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
 {
 System.out.println("vowel");}
@@ -13,5 +15,10 @@ else
 {
 System.out.println("consonence");
 }
+}
+  else
+  {
+    System.out.println("invalid");
+  }
 }
 }
